da38abb9f107bdf3b2449c3f3072ddd245717e48
Kathy Kang <kck3due@virginia.edu>
2022-07-30 23:23:58 -0400

    set up initial dummy server

---
+++ README.md
@@ -0,0 +1,9 @@
+# Instructions for Booting Application
+
+## Start React.js front end
+
+- cd into the `client` folder
+- Type `npm start` in the console to start the front end
+- Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+
---
+++ client/src/components/modals/EmployeeModal.js
@@ -0,0 +1,39 @@
+const { Modal, Box, Typography, Button } = require("@mui/material");
+const React = require("react");
+const { modalBoxStyle } = require("../../styles/mdsStyles/mdsStyles");
+
+const EmployeeModal = ({open, handleClose, fullName=""}) => {
+    return (<Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={modalBoxStyle}>
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          sx={{ marginBottom: "0.5rem" }}
+        >
+          Are you sure you want to delete "{fullName ? fullName : null}"?
+        </Typography>
+        <Button variant="outlined" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => {
+            handleClose();
+            console.log(open);
+          }}
+          sx={{ marginLeft: "0.5rem" }}
+        >
+          Yes
+        </Button>
+      </Box>
+    </Modal>)
+  }
+
+  export default EmployeeModal;
